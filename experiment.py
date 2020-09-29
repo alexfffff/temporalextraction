a100def76a8fe571b64c42c5f9ca6bffe9340e4a
Xuanyu Zhou <zhouxuanyu.personal@gmail.com>
2020-09-29 15:47:53 -0400

    update IO

---
+++ experiment.py
@@ -0,0 +1,62 @@
+import json
+
+from lib_parser import Parser
+
+'''
+Get the verb index from the tags with respect to a specific verb
+'''
+def get_verb_index(tags):
+    for i, t in enumerate(tags):
+        if t == "B-V":
+            return i
+    # There is no verb, abort
+    return -1
+
+'''
+Get the temporal argument as a list of list of tokens
+e.g., "Before I joined the army, I graduated in 2002" will return 
+[
+    ["Before", "I", "joined", "the", "army"],
+    ["in", "2002"],
+]
+'''
+def get_temporal_arguments(words, tags):
+    ret = []
+    for i, t in enumerate(tags):
+        if t == "B-ARGM-TMP":
+            end = -1
+            for j in range(i+1, len(tags)):
+                if tags[j] != "I-ARGM-TMP":
+                    end = j
+                    break
+            ret.append(words[i:end])
+    return ret
+
+
+'''
+Use a preprocessed SRL result to quickly iterate the parser
+So that there is no need to run SRL every single time
+'''
+def run_over_srl_preprocessed_files(file_name, limit=1000):
+    parser = Parser()
+    all_srl_lines = [x.strip() for x in open(file_name).readlines()][:limit]
+    for srl_line in all_srl_lines:
+        srl_objects_in_current_line = json.loads(srl_line)
+        for obj in srl_objects_in_current_line:
+            # Here obj is a SRL model output
+            words = obj['words']
+            # iterate through all verbs (triggers) in the object
+            for verb in obj['verbs']:
+                verb_index = get_verb_index(verb['tags'])
+                if verb_index < 0:
+                    continue
+
+                temporal_arguments = get_temporal_arguments(words, verb['tags'])
+                # Use this next line to see what's inside, comment it out when done
+                # print(temporal_arguments)
+
+                for tmp_arg in temporal_arguments:
+                    result = parser.parse_phrase(tmp_arg)
+
+
+run_over_srl_preprocessed_files("samples/wikipedia_temporal_related_srl_parsed_small.txt")
---
+++ lib_parser.py
@@ -0,0 +1,72 @@
+'''
+An example of the data structure, it's up to you to use it or not
+'''
+class DateStruct:
+    def __init__(self, minute, hour, day, month, year):
+        self.minute = minute
+        self.hour = hour
+        self.day = day
+        self.month = month
+        self.year = year
+
+class ComparativeStruct:
+    def __init__(self):
+        # DIY
+        pass
+
+class Parser:
+    def __init__(self):
+        pass
+
+    '''
+    Input: a list of tokens.
+    e.g., ["in", "2002"]
+    '''
+    def parse_phrase(self, tmp_arg):
+        date_obj = self.parse_date(tmp_arg)
+        fuzzy_date_obj = self.parse_fuzzy_date(tmp_arg)
+        duration_obj = self.parse_duration(tmp_arg)
+        comparative_timepoint = self.parse_comparative_timepoint(tmp_arg)
+        comparative_event = self.parse_comparative_event(tmp_arg)
+        # Then do something
+        # Eventually we want to return something meaningful to the caller
+
+    '''
+    Parse explicit timepoint in the timex
+    e.g., 2002, February, morning, 8 AM
+    '''
+    def parse_reference_date(self, tmp_arg):
+        return None
+
+    '''
+    Parse implicit/fuzzy timepoint in the timex
+    e.g., Friday, Thanksgiving
+    For you to think: how to handle things like ~last Monday~?
+    '''
+    def parse_fuzzy_date(self, tmp_arg):
+        return None
+
+    '''
+    Parse durations in the timex, you may do it later
+    e.g., for 2 hours, for 3 weeks
+    '''
+    def parse_duration(self, tmp_arg):
+        return None
+
+    '''
+    Parse comparative timepoints
+    e.g., yesterday, tomorrow, next week, 2 days ago
+    '''
+    def parse_comparative_timepoint(self, tmp_arg):
+        return None
+
+    '''
+    Parse comparative events, you may do it later
+    e.g., before I graduated
+    '''
+    def parse_comparative_event(self, tmp_arg):
+        return None
+
+    '''
+    Add other functions / cases 
+    '''
