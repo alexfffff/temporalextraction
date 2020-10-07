278c159502aa224e2ebe2731efdf0989b1eefe11
Haoyu Wang <why16gzl@macniece.seas.upenn.edu>
2020-10-06 22:22:16 -0400

    Delete Graph from gurobi_graph.py

--- gurobi_graph.py
+++ gurobi_graph.py
@@ -6,6 +6,7 @@ from gurobipy import *
 import numpy as np
 from collections import defaultdict 
 import math
+
 class gurobi_opt:
     def __init__(self, edges):
         self.why_score = why_score = self.convert_to_score(edges)
@@ -75,43 +76,3 @@ class gurobi_opt:
                 for k in range(output.shape[2]):
                     output[i][j][k] = self.x[i, j, k].X
         return output
-
-#Class to represent a graph 
-class Graph: 
-    def __init__(self,vertices): 
-        self.graph = defaultdict(list) #dictionary containing adjacency List 
-        self.V = vertices #No. of vertices 
-  
-    # function to add an edge to graph 
-    def addEdge(self,u,v): 
-        self.graph[u].append(v) 
-  
-    # A recursive function used by topologicalSort 
-    def topologicalSortUtil(self,v,visited,stack): 
-  
-        # Mark the current node as visited. 
-        visited[v] = True
-  
-        # Recur for all the vertices adjacent to this vertex 
-        for i in self.graph[v]: 
-            if visited[i] == False: 
-                self.topologicalSortUtil(i,visited,stack) 
-  
-        # Push current vertex to stack which stores result 
-        stack.insert(0,v) 
-  
-    # The function to do Topological Sort. It uses recursive  
-    # topologicalSortUtil() 
-    def topologicalSort(self): 
-        # Mark all the vertices as not visited 
-        visited = [False]*self.V 
-        stack =[] 
-  
-        # Call the recursive helper function to store Topological 
-        # Sort starting from all vertices one by one 
-        for i in range(self.V): 
-            if visited[i] == False: 
-                self.topologicalSortUtil(i,visited,stack) 
-  
-        # Print contents of stack 
-        return stack
